5161861fc4fa16b713843aa17578ffe79c509bb0
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-04-29 22:34:53 +0100

    Configurando rutas y opciones del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'gifs',
+        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page')
+
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
