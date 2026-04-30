dff24742be44fb7a374987021c337b32405fe38f
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-04-30 11:59:20 +0100

    Organizando el html de dashboard y enlazando las rutas padres con las hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,26 +2,22 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: 'gifs',
-        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page')
+        path: 'dashboard',
+        loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page'),
+        children: [
+            {
+                path: 'search',
+                loadComponent: () => import('./gifs/pages/search/search')
 
+            },
+            {
+                path: 'trending',
+                loadComponent: () => import('./gifs/pages/trending/trending')
+            }
+        ]
     },
     {
         path: '**',
         redirectTo: 'dashboard'
-    },
-    {
-        path: 'trending'
-
-    },
-    {
-        path: 'search',
-        loadComponent: () => import('./gifs/pages/search/search')
-
-    },
-    {
-        path: 'trending',
-        loadComponent: () => import('./gifs/pages/trending/trending')
     }
-
 ];
--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import Trending from '../trending/trending';
+import Search from '../search/search';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [RouterOutlet, Trending, Search],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.css',
 })
