05688bf8eeb88562f6463eaa705f6cebfe49474b
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-04-30 12:21:38 +0100

    Componetizando el menu

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu-options',
+  imports: [],
+  templateUrl: './side-menu-options.html',
+  styleUrl: './side-menu-options.css',
+})
+export class SideMenuOptions {}
--- src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import Trending from '../trending/trending';
 import Search from '../search/search';
+import { SideMenu } from '../../components/side-menu/side-menu';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet, Trending, Search],
+  imports: [RouterOutlet, Trending, Search, SideMenu],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.css',
 })
