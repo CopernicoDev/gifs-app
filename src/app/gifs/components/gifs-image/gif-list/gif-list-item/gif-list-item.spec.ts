c8aba2f2e3ca443af4d16de110d257068f1f4290
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-05-01 12:56:01 +0100

    Organizando las imagenes en componentes

---
+++ src/app/gifs/components/gifs-image/gif-list/gif-list-item/gif-list-item.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GifListItem } from './gif-list-item';
+
+describe('GifListItem', () => {
+  let component: GifListItem;
+  let fixture: ComponentFixture<GifListItem>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GifListItem],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(GifListItem);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/gifs/pages/trending/trending.ts
+++ src/app/gifs/pages/trending/trending.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GifList } from '../../components/gifs-image/gif-list/gif-list';
 
 @Component({
   selector: 'app-trending',
-  imports: [],
+  imports: [GifList],
   templateUrl: './trending.html',
   styleUrl: './trending.css',
 })
