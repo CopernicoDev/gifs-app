39002571027c83bec7f1de1b85cbde43db622078
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-05-01 12:10:51 +0100

    Modificando el menuOptions para ponerlo como queremos

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface Option {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
+
 
 @Component({
   selector: 'app-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
   styleUrl: './side-menu-options.css',
 })
-export class SideMenuOptions {}
+export class SideMenuOptions {
+
+  menuOptions: Option[] = [
+    {
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+      icon: 'fa-solid fa-chart-line',
+    },
+    {
+      label: 'Buscar',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search',
+      icon: 'fa-solid fa-search',
+    }
+  ]
+}
