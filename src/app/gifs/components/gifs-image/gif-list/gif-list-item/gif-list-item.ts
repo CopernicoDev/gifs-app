8fc9163da43b53d6f54fae876833c487dd33d6d4
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-05-04 12:07:05 +0100

    Haciendo un Prop Drilling mediante inputs

--- src/app/gifs/components/gifs-image/gif-list/gif-list-item/gif-list-item.ts
+++ src/app/gifs/components/gifs-image/gif-list/gif-list-item/gif-list-item.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-gif-list-item',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './gif-list-item.html',
   styleUrl: './gif-list-item.css',
 })
-export class GifListItem {}
+export class GifListItem {
+  imageUrls = input.required<string[]>();
+}
--- src/app/gifs/pages/trending/trending.ts
+++ src/app/gifs/pages/trending/trending.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GifList } from '../../components/gifs-image/gif-list/gif-list';
+
+interface ImgUrls {
+  imageUrls: string[];
+}
+
+
 
 @Component({
   selector: 'app-trending',
@@ -8,4 +14,19 @@ import { GifList } from '../../components/gifs-image/gif-list/gif-list';
   templateUrl: './trending.html',
   styleUrl: './trending.css',
 })
-export default class Trending { }
+export default class Trending {
+  imageUrls = signal<string[]>([
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+  ]);
+}
